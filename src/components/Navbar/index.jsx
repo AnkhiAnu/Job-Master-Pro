c2e147d51dc47128b17cfbcdd3f01eb0cd25ec64
Ananya Saha <ananyasaha282@gmail.com>
2025-01-26 01:08:05 +0530

    head change

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar(){
     return(
         <div className="h-20 flex items-center w-full text-white">
-            <div className="text-3xl pl-20 font-bold">PORTAL</div>
+            <div className="text-3xl pl-20 font-bold">Job Master Pro</div>
              </div>
     )
 }   
